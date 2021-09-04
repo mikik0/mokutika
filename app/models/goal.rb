f2f121d37cc2fc115097caeb35a54ccf2f762f00
mikik0 <usapon_love1222@yahoo.co.jp>
2021-09-04 22:32:50 +0900

    目標参加整備

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,8 +1,15 @@
 class Goal < ApplicationRecord
   validates :title, presence: true
+  validates :content, presence: true
+  validate :deadline_check
   belongs_to :user
   has_many :dones, dependent: :destroy
   has_many :done_users, through: :dones, source: :user
   has_many :follows, dependent: :destroy
   has_many :follow_users, through: :follows, source: :user
+
+  def deadline_check
+    errors.add(:deadline, "は現在より前の日付で登録できません。") if
+      self.deadline < Time.current
+  end
 end
