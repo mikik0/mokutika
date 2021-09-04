4a0a4bfec36ba3ae0bb6afa11d44f48c9463506a
mikik0 <usapon_love1222@yahoo.co.jp>
2021-09-04 21:44:48 +0900

    user周りの整備

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:name]
   has_one_attached :icon
   has_many :goals, dependent: :destroy
   has_many :dones, dependent: :destroy
   has_many :dones_posts, through: :dones, source: :goal
   has_many :follows, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+
 end
