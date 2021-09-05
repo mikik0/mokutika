52fbd6ec18f6dea181212a9d2c5388b98a6c17f8
mikik0 <usapon_love1222@yahoo.co.jp>
2021-09-05 16:37:24 +0900

    投稿にuserを関連付け

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,7 +2,9 @@ class Goal < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validate :deadline_check
+
   has_many :follows, dependent: :destroy
+  belongs_to :user, optional: true #外部キーのnullを許容する
 
   def deadline_check
     errors.add(:deadline, "は現在より前の日付で登録できません。") if
